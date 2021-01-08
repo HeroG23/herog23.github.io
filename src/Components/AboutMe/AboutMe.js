5d51614a74833c32411aa7e7ef58054d33b6a576
Brian Demro <demrobrian@hotmail.com>
2021-01-08 12:04:41 -0600

    finalizing changes

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -2,31 +2,35 @@ import "./AboutMe.css";
 import { Link } from "react-router-dom";
 function AboutMe() {
   return (
-    <div className="About-Me content-box">
-      <h1 className="about-me-header">About Me</h1>
-      <p className="about-me-body">
-        I am a full-stack developer out of Iowa City, IA,
-      </p>
-      <p className="about-me-body">
-        With a passion for sports, video games, and now web development
-      </p>
-      <br />
-      <p className="about-me-body"></p>
-      <p className="about-me-body"></p>
-      <p className="about-me-body"></p>
-
-      <footer>
+    <div className="About-Me">
+      <div className="about-me content-box">
+        <h2 className="about-me-header">About Me</h2>
+        <p className="about-me-body">
+          I am a full-stack developer out of Iowa City, IA, With a passion for
+          sports, video games, and now web development. During a 13 week
+          program, I have gained a varying degree of skills in the web
+          developing field. Starting with learing about Javascript, HTML, and
+          CSS to learning how to build web apps using React, Express and many
+          other attachments to the React foundation, I have built one website,
+          and collaborated with a team to build another.
+        </p>
+        <p className="about-me-body"></p>
+      </div>
+      <footer style={{position:"absolute", bottom:"0", left:"0"}}>
         <h4>Check out my other pages</h4>
         <div className="links">
           <Link
-          className="Links"
-            style={{ textDecoration: "none", color: "white" }}
+            className="Links"
+            style={{ textDecoration: "none", color: "white", border:"1px solid white" }}
             to="/projects"
           >
             Projects
           </Link>
-          <Link className="Links"
-          style={{ textDecoration: "none", color: "white" }} to="/skills">
+          <Link
+            className="Links"
+            style={{ textDecoration: "none", color: "white", border:"1px solid white"  }}
+            to="/skills"
+          >
             Skills
           </Link>
         </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,53 +1,84 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import auth from "../../auth.png";
 import post from "../../post.png";
 import createpost from "../../createpost.png";
-import './Projects.css'
+import "./Projects.css";
 
 function Projects() {
   return (
     <div>
       <div className="Project-Previews content-box">
-        <h1>Projects</h1>
-        <div className="personal-project" style={{border:"2px solid black"}}>
-          <h3>D&D Find Me</h3>
+        <h2 style={{fontSize: "36px", textDecoration:"underline"}}>Projects</h2>
+        <div className="personal-project" style={{ border: "2px solid black" }}>
+          <a href="https://briandemro.dev">
+            <h3>D&D Find Me</h3>
+          </a>
           <div>
-              <h4>Auth page</h4>
+            <h4>Auth page</h4>
             <img
               src={auth}
-              style={{ height: "300px", width: "300px" }}
+              style={{ height: "300px", width: "475px" }}
               alt="auth"
             />
-            <br/>
+            <br />
             <h4>Post page</h4>
             <img
               src={post}
-              style={{ height: "300px", width: "300px" }}
+              style={{ height: "300px", width: "475px" }}
               alt="posts"
             />
-            <br/>
+            <br />
             <h4>Create a post page</h4>
             <img
               src={createpost}
-              style={{ height: "300px", width: "300px" }}
+              style={{ height: "300px", width: "475px" }}
               alt="createPosts"
             />
           </div>
         </div>
-        <div className="group-project"></div>
+        <div className="group-project" style={{border: "2px solid black"}}>
+          <a href="https://dndfindme.com">
+            <h3>DnD Find Me</h3>
+          </a>
+          <div>
+            <h4>Login Page</h4>
+            <video width="70%" alt="login" controls autoplay>
+              <source src="login.mp4" type="video/mp4" />
+            </video>
+            <h4>Registration Page</h4>
+            <img src="register1.jpg" alt="group-register" style={{width: "475px", height:"300px"}}/>
+            <h4>Feed</h4>
+            <video width="70%" alt="feed" controls autoplay>
+              <source src="feed.mp4" type="video/mp4" />
+            </video>
+            <h4>Single Post</h4>
+            <img src="singlePost.png" alt="single-post" style={{height:"300px", width:"475px"}}/>
+            <h4>New Post</h4>
+            <img src="newPost.jpg" alt="new-post" style={{height:"300px", width:"475px"}}/>
+            <h4>Google Maps</h4>
+            <video width="70%" alt="feed" controls autoplay>
+              <source src="googleMaps.mp4" type="video/mp4" />
+            </video>
+            <h4>Profile</h4>
+            <img src="profile.jpg" alt="profile" style={{height:"300px", width:"475px"}}/>
+          </div>
+        </div>
       </div>
-      <footer>
+      <footer style={{position:"relative", bottom:"0"}}>
         <h4>Check out my other pages</h4>
         <div className="links">
           <Link
-          className="Links"
-            style={{ textDecoration: "none", color: "white" }}
+            className="Links"
+            style={{ textDecoration: "none", color: "white", border:"1px solid white" }}
             to="/"
           >
             About Me
           </Link>
-          <Link className="Links"
-          style={{ textDecoration: "none", color: "white" }} to="/skills">
+          <Link
+            className="Links"
+            style={{ textDecoration: "none", color: "white", border:"1px solid white"}}
+            to="/skills"
+          >
             Skills
           </Link>
         </div>
